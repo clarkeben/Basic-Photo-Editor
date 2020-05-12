152fb81cebd71e59cbe6786a4c5e10e82346b20b
Ben <benclarke023@gmail.com>
2020-05-13 00:13:59 +0100

    Initial Commit

---
+++ Photo Library/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Library
+//
+//  Created by Ben Clarke on 13/05/2020.
+//  Copyright © 2020 Ben Clarke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
